db300f7b940c39b1fec5cc9cdcae676dfe64ad32
Whetu Paitai <whetu@pikistudios.com>
2023-01-23 14:46:04 +1300

    test base addition

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,54 +1,55 @@
 export default {
-    themeConfig: {
-        logo: '/assets/images/logo.png',
-        siteTitle: 'Lorecraft',
+  base: "lorestudios.github.io",
+  themeConfig: {
+    logo: "/assets/images/logo.png",
+    siteTitle: "Lorecraft",
 
-        algolia: {
-            appId: '...',
-            apiKey: '...',
-            indexName: '...'
-        },
+    algolia: {
+      appId: "...",
+      apiKey: "...",
+      indexName: "...",
+    },
 
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Page One', link: '/documentation/page-one' },
-            { text: 'Page Two', link: '/documentation/page-two' },
-            {
-                text: 'Pages',
-                items: [
-                    { text: 'Home ', link: '/' },
-                    { text: 'Page One', link: '/documentation/page-one' },
-                    { text: 'Page Two', link: '/documentation/page-two' },
-                    { text: 'Team Page', link: '/team/team-page' },
-                ]
-            }
+    nav: [
+      {text: "Home", link: "/"},
+      {text: "Page One", link: "/documentation/page-one"},
+      {text: "Page Two", link: "/documentation/page-two"},
+      {
+        text: "Pages",
+        items: [
+          {text: "Home ", link: "/"},
+          {text: "Page One", link: "/documentation/page-one"},
+          {text: "Page Two", link: "/documentation/page-two"},
+          {text: "Team Page", link: "/team/team-page"},
         ],
-        sidebar: [
-            {
-                text: 'Doc Page',
-                collapsible: true,
-                items: [
-                    { text: 'Home', link: '/documentation/' },
-                    { text: 'Page one', link: '/documentation/page-one' },
-                    { text: 'Page two', link: '/documentation/page-two' },
-                ]
-            },
-            {
-                text: 'Team Page',
-                collapsible: true,
-                items: [
-                    { text: 'Home', link: '/team/' },
-                    { text: 'Team Page', link: '/team/team-page' },
-                ]
-            }, 
-            {
-                text: 'Integreation',
-                collapsible: true,
-                items: [
-                    { text: 'Introduction', link: '/introduction' },
-                    { text: 'Getting Started', link: '/getting-started' },
-                ]
-            }
+      },
+    ],
+    sidebar: [
+      {
+        text: "Doc Page",
+        collapsible: true,
+        items: [
+          {text: "Home", link: "/documentation/"},
+          {text: "Page one", link: "/documentation/page-one"},
+          {text: "Page two", link: "/documentation/page-two"},
         ],
-    }
-}
+      },
+      {
+        text: "Team Page",
+        collapsible: true,
+        items: [
+          {text: "Home", link: "/team/"},
+          {text: "Team Page", link: "/team/team-page"},
+        ],
+      },
+      {
+        text: "Integreation",
+        collapsible: true,
+        items: [
+          {text: "Introduction", link: "/introduction"},
+          {text: "Getting Started", link: "/getting-started"},
+        ],
+      },
+    ],
+  },
+};
